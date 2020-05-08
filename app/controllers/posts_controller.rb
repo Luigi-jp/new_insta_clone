13cd0eaa57c5111419f29e795ee85463870b04b2
EC2 Default User <ec2-user@ip-172-31-43-220.ap-northeast-1.compute.internal>
2020-05-08 01:45:34 +0000

    リンクのアイコン化とPostの削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: :destroy
   
   def new
     @post = Post.new
@@ -15,11 +16,18 @@ class PostsController < ApplicationController
   end
   
   def destroy
-    
+    @post.destroy
+    flash[:success] = "削除しました。"
+    redirect_to request.referrer || root_url
   end
   
   private
     def post_params
       params.require(:post).permit(:picture)
+    end
+    
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
